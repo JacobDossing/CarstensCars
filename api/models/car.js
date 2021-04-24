d215eed9d651c386dec6c71f55fbe6c0647a0b2d
jacobdr-eamv <jaco198r@eamv.dk>
2021-04-24 12:08:33 +0200

    Jacobs fine CRUD opdateringer

--- api/models/car.js
+++ api/models/car.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const carSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    brand: { type: String, require: true },
-    model: { type: String, require: true },
+    brand: { type: String, required: true },
+    model: { type: String, required: true },
     price: { type: Number, default: 150000 }
 });
 
--- api/routes/cars.js
+++ api/routes/cars.js
@@ -6,15 +6,20 @@ const Car = require('../models/car');
 
 router.get('/', (req, res, next) => {
     Car.find()
-    .exec()
-    .then(docs => {
-        res.status(200).json(docs);
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
+        .select('brand model price')
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                cars: docs
+            };
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
         });
-    });
 });
 
 router.post('/', (req, res, next) => {
@@ -25,17 +30,99 @@ router.post('/', (req, res, next) => {
         price: req.body.price
     });
     car.save()
-    .then(result => {
-        res.status(201).json({
-            message: 'Handling POST requests to /cars',
-            createdCar: car
+        .then(result => {
+            res.status(201).json({
+                message: 'Handling POST requests to /cars',
+                createdCar: car
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
         });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
+});
+
+router.get('/:carId', (req, res, next) => {
+    const id = req.params.carId;
+    Car.findById(id)
+        .select('brand model price')
+        .exec()
+        .then(doc => {
+            if (doc) {
+                res.status(200).json({
+                    car: doc
+                });
+            } else {
+                res.status(404).json({
+                    message: "Ingen biler med det angivne ID",
+                    request: {
+                        hint: "Få evt. en oversigt over alle biler med følgende link:",
+                        type: "GET",
+                        url: "http://localhost:1337/cars"
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+
+router.patch('/:carId', (req, res, next) => {
+    const id = req.params.carId;
+    //Update operationer (de attributter, der skal opdateres)
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Car.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            if (result) {
+                res.status(200).json({
+                    message: 'Opdatering successfuld'
+                })
+            } else {
+                res.status(404).json({
+                    message: "Der blev ikke foretaget nogen opdatering",
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+
+router.delete('/:carId', (req, res, next) => {
+    const id = req.params.carId;
+    Car.findById(id)
+        .exec()
+        .then(doc => {
+            if (doc) {
+                Car.remove({ _id: id })
+                    .exec()
+                    .then(result => {
+                        res.status(200).json({
+                            message: 'Bil slettet',
+                            car: doc
+                        })
+                    })
+            } else {
+                res.status(404).json({
+                    message: 'Ingen bil med id ' + id
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 module.exports = router;
